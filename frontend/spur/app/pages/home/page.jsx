7936e263300fabb711b2a650ebe872714b4abc87
rutdvaj <54937822+rutdvaj@users.noreply.github.com>
2026-01-01 14:16:16 +0530

    fe commit-1

--- frontend/spur/app/pages/home/page.jsx
+++ frontend/spur/app/pages/home/page.jsx
@@ -1,6 +1,6 @@
 import ChatContainer from "../../chat/chatcontainer";
 
-export default function Home() {
+export default function HomePage() {
   return (
     <main className="min-h-screen flex items-center justify-center p-4 bg-background">
       <div className="w-full max-w-4xl h-[90vh] max-h-200">
