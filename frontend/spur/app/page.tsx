9e29a0a1a13846b1cddaacf7303f39def46995ca
rutdvaj <54937822+rutdvaj@users.noreply.github.com>
2026-01-01 16:14:09 +0530

    vc1

--- frontend/spur/app/page.tsx
+++ frontend/spur/app/page.tsx
@@ -1,9 +1,6 @@
 import HomePage from "../app/pages/home/page";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div>
-      <HomePage />
-    </div>
-  );
+  return redirect("pages/home");
 }
